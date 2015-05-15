34095456fdc422854db9963e44c991bcbe9feac0
T-rain <travis965@gmail.com>
2015-05-16 04:42:02 +0800

    hw11-5,pgproblem

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,7 +1,7 @@
 class Cost < ActiveRecord::Base
 	belongs_to :user
 	def self.dashboard
-		data = Cost.select("title,money as num").group("title")
+		data = Cost.select("title,sum(money) as num").group("title")
 		data.map{|d|[d.title,d.num]}
 		#data.map{|d|[Group.find(d.group_id).name,d.total]}
 	end
